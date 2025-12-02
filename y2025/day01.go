227afb3fa1cbde304f65c445945a1adf198e795c
Josh Holman <git@jolman.me>
2025-12-01 17:34:42 -0800

    cleanup

--- y2025/day01.go
+++ y2025/day01.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
+var lines []string
+
 func main() {
-	content, err := os.ReadFile("inputs/day01")
+	file, err := os.Open("inputs/day01")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(string(content))
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	part1()
+}
+
+func part1() {
+	password := 0
+	dial := 50
+
+	for _, line := range lines {
+		direction := string(line[0])
+		amount, _ := strconv.Atoi(string(line[1:]))
+		switch direction {
+		case "L":
+			dial -= amount
+		case "R":
+			dial += amount
+		}
+	}
+	fmt.Printf("Password: %d\n", password)
 }
